9928a96ee3b4cb78961471df6fba289e80199a57
evelyncaraujo <evyaraujoifc@gmail.com>
2024-11-28 16:43:33 -0300

    Rota da página login

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,24 @@
-i
+import { createRouter, createWebHistory } from 'vue-router'
+import loginPageView from '@/views/loginPageView.vue'
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: '/',
+        component: BlankLayout,
+        children: [
+          {
+            path: '',
+            name: 'login',
+            component: loginPageView
+          },
+        ]
+    },
+
+  
+  ]
+})
+
+export default router

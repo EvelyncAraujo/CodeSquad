415c339dadc113b5a5a8efc1115f457759b6b24b
evelyncaraujo <evyaraujoifc@gmail.com>
2024-11-29 20:41:13 -0300

    página gerenciamento 80%

--- src/views/gerenciamentoAcademicoView.vue
+++ src/views/gerenciamentoAcademicoView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed } from "vue";
+import AsideComponent from '@/components/AsideComponent.vue'
 
 const isDarkMode = ref(false);
 const toggleDarkMode = () => {
@@ -88,17 +89,9 @@ const applyFilters = () => {
 
 <template>
   <div class="page">
-    <aside class="sidebar">
-      <img src="/src/assets/logo.png" alt="Logo" />
-
-      <ul class="menu">
-        <li>Explore</li>
-        <li>Conselho</li>
-        <li>Gerenciamento Acadêmico</li>
-      </ul>
-      <button class="logout">Sair</button>
-    </aside>
-
+    <div class="aside">
+      <AsideComponent/>
+    </div>
     <main class="content">
       <div class="user-info">
         <img
@@ -174,6 +167,9 @@ const applyFilters = () => {
 </template>
 
 <style scoped>
+  .aside{
+    min-width: 240px;
+  }
 .page {
   display: flex;
   transition: background 0.3s, color 0.3s;
@@ -212,14 +208,6 @@ const applyFilters = () => {
   margin-left: 2rem;
 }
 
-.logout {
-  background: red;
-  color: white;
-  border: none;
-  padding: 10px;
-  border-radius: 5px;
-}
-
 /* Conteúdo principal */
 .content {
   flex: 1;
@@ -239,6 +227,7 @@ const applyFilters = () => {
 }
 .info {
   margin-left: 5rem;
+ 
 }
 .search {
   float: right;
@@ -251,7 +240,16 @@ const applyFilters = () => {
   border-radius: 2rem;
   background-color: #c2bfbf1f;
   padding: 2rem;
+  width: 65rem;
 }
+option{
+  background-color: #f8f5f521;
+  color: black;
+  border: none;
+  border-radius: 20px;
+}
+
+
 .theme-toggle {
   position: fixed;
   margin-left: 85rem;
@@ -273,7 +271,7 @@ const applyFilters = () => {
   padding: 10px 20px;
   border: 1px solid rgba(226, 174, 210, 1);
   border-radius: 20px;
-  color: white;
+  color: rgb(255, 255, 255);
   background-color: rgba(226, 174, 210, 1);
 }
 

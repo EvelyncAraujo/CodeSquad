e769ec3e631ca83e8ceff3d1c9f3915dbfe09da0
Wecreds <Wecredsbr@gmail.com>
2024-11-30 02:36:37 -0300

    feat(resetPassword):fazendo service e store

---
+++ src/stores/resetPassword.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+
+import PasswordResetService from '@/services/passwordReset'
+const passwordResetService = new PasswordResetService()
+
+export const usePasswordResetStore = defineStore('passwordReset', () => {
+  const requestPasswordReset = async email => {
+    const response = await passwordResetService.sendResetPasswordCode(email)
+
+    return response
+  }
+
+  const confirmPasswordReset = async (newPassword, token, email) => {
+    const response = await passwordResetService.resetPassword(
+      newPassword,
+      token,
+      email,
+    )
+
+    return response
+  }
+
+  return {
+    requestPasswordReset,
+    confirmPasswordReset,
+  }
+})

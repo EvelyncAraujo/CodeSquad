1ca2dd23f573a8906af8af06553092edf4e6e7c2
giulianobre <giuliarnobre@gmail.com>
2024-11-29 21:21:46 -0300

    Alterações de fidelidade no pop-up de esquecer

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,7 +7,7 @@
 <style>
 .logo {
   position: sticky;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0);
   top: 0;
   left: 0;
   width: 100%;
--- src/views/novaSenhaView.vue
+++ src/views/novaSenhaView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, computed } from "vue";
-import Swal from "sweetalert2";
+
 
 import { useRouter } from 'vue-router';
 
